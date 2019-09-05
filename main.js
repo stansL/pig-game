4f17abdcbad4c53a2ec45b398ce8f98a1bb6f53c
Stanslaus <mstans8@gmail.com>
2019-09-05 13:13:30 +0300

    Single and multiline comments

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ var lastName = "Odhiambo";
 var age = 32;
 var fullAge = true;
 
-
+//This is a single line comment
+/*
+A multiline comment here
+That spans multiple lines
+*/
 console.log(firstName, lastName, age, fullAge);
+
+
