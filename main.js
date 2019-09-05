231f2f79cb7fa3c7edb6db9bfa1142df7eef8238
Stanslaus <mstans8@gmail.com>
2019-09-05 13:56:42 +0300

    demonstrate Truthy and Falsy values

--- main.js
+++ main.js
@@ -14,3 +14,32 @@ console.log(firstName, lastName, age, fullAge);
 console.log(firstName + ' ' + age);
 
 
+//Variable Mutation - changing the value/type of a variable
+age = 'twenty eight';
+
+console.log(firstName, lastName, age, fullAge);
+console.log(firstName + ' ' + age);
+
+
+/* Basic Math Operators */
+var currentYear = 2019;
+var yearJohn = currentYear - 32;
+var yearMark = currentYear - 28;
+
+console.log(yearMark);
+
+//Falsy values: undefined, null, 0, '', NaN
+//Truthy values: all values considered true...aka..non-falsy values
+
+var height = 23;
+
+if(height){
+    console.log('Variable is defined');
+}else{
+        console.log('Variable has NOT been defined');
+}
+
+
+
+
+
