2280559ac6b318ededd543bc7b1396b62c988769
Stanslaus <mstans8@gmail.com>
2019-09-07 09:30:01 +0300

    added array detailsl

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/********************************
+* Arrays
+*/
+
+var names = ['John', 'Mark','Jane'];
+var years = new Array(1990,1961,1948);
+
+//zero based access
+
+console.log(names[10]);
+console.log(names);
+console.log(names.length);
+
+var john = ['John', 'Smith', 1990, 'teacher',false];
+
+console.log(john);
