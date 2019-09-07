ea55668d391d4c5b3b442f8a9e738dab39e32f97
Stanslaus <mstans8@gmail.com>
2019-09-07 08:44:37 +0300

    introduction to functions

---
+++ functions.js
@@ -0,0 +1,30 @@
+/**
+* Functions
+*
+**/
+
+function calculateAge(yearOfBirth){
+    var currentYear = 2019;
+    return currentYear  - yearOfBirth;
+    
+}
+
+console.log(calculateAge(1987));
+
+
+
+function yearsToRetire(yearOfBirth, firstName){
+    var age = calculateAge(yearOfBirth);
+    var retirementAge = 65 - age;
+    if(retirementAge > 0){ 
+        console.log(firstName, 'retires in ', retirementAge,'years');
+    }else{
+        console.log(firstName, 'is already retired!');
+
+    }
+    
+    
+}
+
+
+yearsToRetire(1947,'Stanslaus');
